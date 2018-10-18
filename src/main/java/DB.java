07d9fb2749e3901b35cc5cf74420c5744667dc76
zecollokaris <collo.kariss@gmail.com>
2018-10-18 14:57:44 +0300

    Add Database

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,7 +1,6 @@
 import org.sql2o.Sql2o;
 
 public class DB {
-
-     public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/leaderboard", "karis", "Kar!s123");
+     public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-107-20-211-10.compute-1.amazonaws.com:5432/db29922g69lbh4", "qqifdjiuyemlav", "96797e7c6c6fb3e813e3636a4bc4bbb1e4924dd2b80b8a3e999e01b07f76bb56");
 
  }
